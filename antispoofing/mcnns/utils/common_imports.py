c41c79cd81f80d8b86f19dc7bdcbbd8053a29cf0
richard <1042651820@qq.com>
2019-12-12 16:53:02 +0800

    random

--- antispoofing/mcnns/utils/common_imports.py
+++ antispoofing/mcnns/utils/common_imports.py
@@ -2,7 +2,7 @@
 from antispoofing.mcnns.utils.constants import SEED
 
 import tensorflow as tf
-tf.random.set_seed(SEED)
+tf.set_random_seed(SEED)
 
 import numpy as np
 import matplotlib.pyplot as plt
